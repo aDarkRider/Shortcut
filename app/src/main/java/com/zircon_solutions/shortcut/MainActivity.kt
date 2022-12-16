8612cc4d9277a73ab09b81c786d51a01c05dd9a2
aDarkRider <adnanaslam096@gmail.com>
2022-12-16 16:46:01 +0500

    fixed issues

--- app/src/main/java/com/zircon_solutions/shortcut/MainActivity.kt
+++ app/src/main/java/com/zircon_solutions/shortcut/MainActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import com.zircon_solutions.shortcut.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,17 +17,40 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
+        mBinding.business.setOnClickListener {
+            mBinding.business.isChecked = true
+            mBinding.whatsapp.isChecked = false
+        }
+
+        mBinding.whatsapp.setOnClickListener {
+            mBinding.business.isChecked = false
+            mBinding.whatsapp.isChecked = true
+        }
+
+        mBinding.ccp.registerCarrierNumberEditText(mBinding.phone)
+
         mBinding.submit.setOnClickListener {
             val phone = mBinding.phone.text.toString().trim()
             if (phone.isEmpty()) {
                 mBinding.phone.error = "Enter Phone Number"
                 mBinding.phone.requestFocus()
             } else {
+                val phoneNumber = mBinding.ccp.fullNumber
                 try {
-                    val uri = Uri.parse("https://api.whatsapp.com/send?phone=${phone}")
-                    val waIntent = Intent(Intent.ACTION_VIEW).setData(uri)
-                    startActivity(waIntent)
-                    mBinding.phone.setText("")
+                    val uri = Uri.parse("https://api.whatsapp.com/send?phone=${phoneNumber}")
+                    if (mBinding.business.isChecked) {
+                        Toast.makeText(this, "Business", Toast.LENGTH_SHORT).show()
+                        val waIntent = Intent(Intent.ACTION_VIEW).setData(uri)
+                        waIntent.setPackage("com.whatsapp.w4b")
+                        startActivity(waIntent)
+                        mBinding.phone.setText("")
+                    } else {
+                        Toast.makeText(this, "Whatsapp", Toast.LENGTH_SHORT).show()
+                        val waIntent = Intent(Intent.ACTION_VIEW).setData(uri)
+                        waIntent.setPackage("com.whatsapp")
+                        startActivity(waIntent)
+                        mBinding.phone.setText("")
+                    }
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
